449c81cf8f9882f4550813eb32dc302584a09224
S5577727 <115453720+S5577727@users.noreply.github.com>
2022-11-21 20:03:36 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,22 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int n;
+    long long a=1,b=1;
+    cin >> n;
+    if (n>=2){
+        for (int i=0; i<n; i++){
+            if(i<n){
+                cout << a << endl;
+                }
+            i++;
+            if(i<n){
+                cout << b << endl;
+                }
+            a+=b;
+            b+=a;
+        }
+    }
+    else (cout << "errore");
+    return 0;
 }
